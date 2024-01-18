1ef1f1db49bd8ad80cbdc28328d2a76ade6d1e4c
AropetJ <aropetjoel32@gmail.com>
2024-01-18 17:57:41 +0100

    Attempted task 4 - buy buy buy

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,4 +1,4 @@
--- SQL script that creates a trigger that resets the attribute
+-- An SQL script that creates a trigger that resets the attribute
 -- valid_email only when the email has been changed.
 DROP TRIGGER IF EXISTS validate_email;
 DELIMITER $$
--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,24 +1,23 @@
---
-DROP PROCEDURE IF EXISTS AddBonus;
-DELIMITER $$
-CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
+-- An SQL script that creates a stored procedure AddBonus
+-- that adds a new correction for a student.
+CREATE PROCEDURE AddBonus(
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
+)
 BEGIN
-    DECLARE project_count INT DEFAULT 0;
-    DECLARE project_id INT DEFAULT 0;
+    DECLARE project_id INT;
 
-    SELECT COUNT(id)
-        INTO project_count
-        FROM projects
-        WHERE name = project_name;
-    IF project_count = 0 THEN
-        INSERT INTO projects(name)
-            VALUES(project_name);
+    -- Check if project_name exists in projects table
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+
+    -- If project_name does not exist, create a new project
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
     END IF;
-    SELECT id
-        INTO project_id
-        FROM projects
-        WHERE name = project_name;
-    INSERT INTO corrections(user_id, project_id, score)
-        VALUES (user_id, project_id, score);
-END $$
-DELIMITER ;
+
+    -- Insert the new correction for the student
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END;
+
