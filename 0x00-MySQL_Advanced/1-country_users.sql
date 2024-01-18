fd53a494afdc47b47f30afda9cea0368ad27347b
AropetJ <aropetjoel32@gmail.com>
2024-01-18 17:14:06 +0100

    Attempted task 2 - in and out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Creates a table for country users
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+);
