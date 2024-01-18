3b21c7a3d9a3d375bfa4e0cdd652e52b0756bbd8
AropetJ <aropetjoel32@gmail.com>
2024-01-18 18:17:28 +0100

    Attempted task 10

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,4 +1,5 @@
---
+-- An SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that
+-- computes and store the average weighted score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
