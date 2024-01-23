4b948015d60dfb395d159e68c58ba9f24c096944
AropetJ <aropetjoel32@gmail.com>
2024-01-23 20:21:51 +0100

    Attempted task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,11 +4,6 @@
 def insert_school(mongo_collection, **kwargs):
     """
     Inserts a new document in a collection based on kwargs.
-    Args:
-        mongo_collection (pymongo.collection.Collection):
-        The pymongo collection object.
-        **kwargs: Keyword arguments representing the fields
-        and values of the document.
     Returns:
         str: The new _id of the inserted document.
     """
