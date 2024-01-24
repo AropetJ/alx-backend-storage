0dddd0250260ac94024277a82ae5488260ecbdf7
AropetJ <aropetjoel32@gmail.com>
2024-01-24 20:15:35 +0100

    Created the project repository

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,44 +4,47 @@
 import requests
 import functools
 import time
+import redis
 
+redis_client = redis.Redis()
 
 def cache_decorator(func):
     '''
-    Decorator function that caches the response of a web page for 10 seconds.
+    Decorator function that caches the response of a web page for 10 seconds
+    and tracks the number of times a URL was accessed using Redis.
     Args:
         func (function): The function to be decorated.
     Returns:
         function: The decorated function.
     '''
-    cache = {}
-
     @functools.wraps(func)
-    def wrapper(url):
+    def wrapper(url) -> str:
         '''
         Wrapper function that checks if the response for a given URL is already
-        cached. If it is, it returns the cached response. Otherwise, it calls
-        the original function to fetch the response, caches it, and returns it.
+        cached in Redis. If it is, it returns the cached response. Otherwise, it calls
+        the original function to fetch the response, caches it in Redis, and returns it.
+        It also tracks the number of times the URL was accessed in Redis.
         Args:
             url (str): The URL of the web page.
         Returns:
             str: The content of the web page.
         '''
-        if url in cache and cache[url]['expiration'] > time.time():
-            return cache[url]['content']
+        count_key = f'count:{url}'
+        cached_response = redis_client.get(url)
+        if cached_response:
+            redis_client.incr(count_key)
+            return cached_response.decode()
 
         response = func(url)
-        cache[url] = {
-            'content': response,
-            'expiration': time.time() + 10
-        }
+        redis_client.setex(url, 10, response)
+        redis_client.incr(count_key)
         return response
 
     return wrapper
 
 
 @cache_decorator
-def get_page(url):
+def get_page(url: str) -> str:
     '''
     Fetches the content of a web page.
     Args:
