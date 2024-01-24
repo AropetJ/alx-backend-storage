a94e17a41fd13fbd8d406e754fdad3b1ecb714c8
AropetJ <aropetjoel32@gmail.com>
2024-01-24 20:29:20 +0100

    Created the project repository

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,45 +1,34 @@
 #!/usr/bin/env python3
-'''web.py'''
-
-import requests
-import functools
-import time
+'''web.py
+'''
 import redis
+import requests
+from functools import wraps
 from typing import Callable
 
 redis_client = redis.Redis()
 
 
-def cache_decorator(func: Callable) -> Callable:
+def cache_decorator(method: Callable) -> Callable:
     '''
     Decorator function that caches the response of a web page for 10 seconds
     and tracks the number of times a URL was accessed using Redis.
-    Args:
-        func (function): The function to be decorated.
-    Returns:
-        function: The decorated function.
     '''
-    @functools.wraps(func)
+    wraps(method)
     def wrapper(url) -> str:
         '''
         Wrapper function that checks if the response for a given URL is already
-        cached in Redis. If it is, it returns the cached response. Otherwise, it calls
-        the original function to fetch the response, caches it in Redis, and returns it.
-        It also tracks the number of times the URL was accessed in Redis.
-        Args:
-            url (str): The URL of the web page.
-        Returns:
-            str: The content of the web page.
+        cached in Redis.
         '''
         redis_client.incr(f'count:{url}')
         cached_response = redis_client.get(f'cached:{url}')
         if cached_response:
             return cached_response.decode('utf-8')
 
-        response = func(url)
+        cached_response = func(url)
         redis_client.set(f'count:{url}', 0)
         redis_client.setex(f'result:{url}', 10, cached_response)
-        return response
+        return cached_response
     return wrapper
 
 
@@ -47,10 +36,6 @@ def cache_decorator(func: Callable) -> Callable:
 def get_page(url: str) -> str:
     '''
     Fetches the content of a web page.
-    Args:
-        url (str): The URL of the web page.
-    Returns:
-        str: The content of the web page.
     '''
     response = requests.get(url)
     return response.text
