df665e037fa99b0651ba42913f5e5698f9285315
AropetJ <aropetjoel32@gmail.com>
2024-01-26 22:24:40 +0100

    Attempted task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,43 +1,32 @@
 #!/usr/bin/env python3
-'''web.py
-'''
+""" Redis Module """
+
+from functools import wraps
 import redis
 import requests
-from functools import wraps
 from typing import Callable
 
-redis_client = redis.Redis()
+redis_ = redis.Redis()
 
 
-def cache_decorator(method: Callable) -> Callable:
-    '''
-    Decorator function that caches the response of a web page for 10 seconds
-    and tracks the number of times a URL was accessed using Redis.
-    '''
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
     @wraps(method)
-    def wrapper(url) -> str:
-        '''
-        Wrapper function that checks if the response for a given URL is
-        already cached in Redis.
-        '''
-        @wraps(method)
-        def wrapper(url):
-            """ Wrapper for decorator """
-            redis_client.incr(f"count:{url}")
-            cached_html = redis_client.get(f"cached:{url}")
-            if cached_html:
-                return cached_html.decode('utf-8')
-            html = method(url)
-            redis_client.setex(f"cached:{url}", 10, html)
-            return html
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
 
-        return wrapper
+    return wrapper
 
 
-@cache_decorator
+@count_requests
 def get_page(url: str) -> str:
-    '''
-    Fetches the content of a web page.
-    '''
+    """ Obtain the HTML content of a  URL """
     req = requests.get(url)
     return req.text
